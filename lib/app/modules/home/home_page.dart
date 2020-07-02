dc1dfbb5594453d9e10aefec1a063574ddedc427
Lucas <joselucas0303@gmail.com>
2020-07-02 19:43:17 -0300

    order_by dropdown added. not functionaly yet

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -5,6 +5,9 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_music_player/app/modules/home/controller/home_controller.dart';
 import 'package:flutter_music_player/app/modules/home/utils/widgets/navigator_items.dart';
 
+import 'utils/constants/order_by_enum.dart';
+import 'utils/widgets/dropdown_items.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,31 +15,39 @@ class HomePage extends StatelessWidget {
 
     return Observer(
       builder: (_) => Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(
-              Icons.filter_list,
-            ),
-            onPressed: () {},
-            color: Colors.black,
+          backgroundColor: Colors.white,
+          appBar: homeController.showAppBar
+              ? AppBar(
+                  leading: Padding(
+                    padding: const EdgeInsets.all(4.5),
+                    child: DropdownButton<OrderBy>(
+                      isExpanded: true,
+                      icon: Container(),
+                      underline: Container(),
+                      items: dropDownItems(),
+                      onChanged: homeController.onChanged,
+                      hint: Icon(
+                        Icons.list,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  elevation: 0,
+                  backgroundColor: Colors.white,
+                )
+              : null,
+          bottomNavigationBar: CustomNavigationBar(
+            backgroundColor: Colors.white,
+            selectedColor: Colors.orange,
+            strokeColor: Colors.orange,
+            elevation: 20.0,
+            currentIndex: homeController.currentIndex,
+            scaleCurve: Curves.easeInOutCirc,
+            scaleFactor: 0.5,
+            onTap: homeController.changeCurrentIndex,
+            items: navigatorItems(),
           ),
-          elevation: 0,
-          backgroundColor: Colors.white,
-        ),
-        bottomNavigationBar: CustomNavigationBar(
-          backgroundColor: Colors.white,
-          selectedColor: Colors.orange,
-          strokeColor: Colors.orange,
-          elevation: 20.0,
-          currentIndex: homeController.currentIndex,
-          scaleCurve: Curves.easeInOutCirc,
-          scaleFactor: 0.5,
-          onTap: homeController.changeCurrentIndex,
-          items: navigatorItems(),
-        ),
-        body: homeController.currentPage,
-      ),
+          body: homeController.currentPage),
     );
   }
 }
