e6b80067a4f6cd825a070a2bbab6487ac5775002
Lucas <joselucas0303@gmail.com>
2020-07-03 11:43:53 -0300

    album attribute removed

--- lib/app/modules/home/models/song.dart
+++ lib/app/modules/home/models/song.dart
@@ -2,19 +2,13 @@ import 'package:flutter/material.dart';
 
 class Song {
   String name;
-  String album;
   String path;
   bool isFavorite;
 
-  Song(
-      {@required this.name,
-      @required this.album,
-      @required this.path,
-      @required this.isFavorite});
+  Song({@required this.name, @required this.path, @required this.isFavorite});
 
   Song.fromJson(Map data) {
     name = data['title'];
-    album = data['album'];
     path = data['path'];
     isFavorite = data['isFavorite'];
   }
