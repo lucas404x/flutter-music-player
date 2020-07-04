9f625efaa99b2c601ac66733b5b6fdc294ada045
Lucas <joselucas0303@gmail.com>
2020-07-04 18:36:13 -0300

    dispose added on dispose page method

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -5,11 +5,22 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_music_player/app/modules/home/controller/home_controller.dart';
 import 'package:flutter_music_player/app/modules/home/utils/widgets/navigator_items.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final homeController = Modular.get<HomeController>();
+
+  @override
+  void dispose() {
+    homeController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final homeController = Modular.get<HomeController>();
-
     return Observer(
       builder: (_) => Scaffold(
           bottomNavigationBar: CustomNavigationBar(
