3f2b58e48882e15c7eee3ccddadfefbb882d724a
Lucas <joselucas0303@gmail.com>
2020-07-03 22:33:55 -0300

    icons from Navigator Bar changed

--- lib/app/modules/home/utils/widgets/navigator_items.dart
+++ lib/app/modules/home/utils/widgets/navigator_items.dart
@@ -2,9 +2,10 @@ import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 List<CustomNavigationBarItem> navigatorItems() => [
-      CustomNavigationBarItem(icon: Icons.list, selectedTitle: 'Songs'),
       CustomNavigationBarItem(
-          icon: Icons.playlist_play, selectedTitle: 'Playlists'),
+          icon: Icons.library_music, selectedTitle: 'Songs'),
+      CustomNavigationBarItem(
+          icon: Icons.queue_music, selectedTitle: 'Playlists'),
       CustomNavigationBarItem(icon: Icons.play_arrow, selectedTitle: 'Play'),
       CustomNavigationBarItem(icon: Icons.favorite, selectedTitle: 'Favorites'),
       CustomNavigationBarItem(icon: Icons.settings, selectedTitle: 'Settings')
