0eddab6c671ae3525adcf2b4247f2deda0152348
Lucas <joselucas0303@gmail.com>
2020-07-07 13:01:55 -0300

    PLAYLIST_KEY constant added

--- lib/app/modules/home/utils/constants/constants.dart
+++ lib/app/modules/home/utils/constants/constants.dart
@@ -4,4 +4,5 @@ class Constants {
   static const FIRST_MAIN_COLOR = Color(0xFFFFA82E);
   static const SECOND_MAIN_COLOR = Color(0xFFE04A2A);
   static const FAVORITE_KEY = 'favorites';
+  static const PLAYLIST_KEY = 'playlist';
 }
