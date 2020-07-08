3f6d96ddce16a08ccde63e3d67d9c109045e1d1b
Lucas <joselucas0303@gmail.com>
2020-07-08 13:18:40 -0300

    now size of bottom_sheet changes dynamically

--- lib/app/modules/home/pages/playlist/controller/playlist_controller.dart
+++ lib/app/modules/home/pages/playlist/controller/playlist_controller.dart
@@ -1,8 +1,40 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_music_player/app/modules/home/stores/home_store.dart';
+import 'package:flutter_music_player/app/modules/home/utils/constants/constants.dart';
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 part 'playlist_controller.g.dart';
 
 class PlayListController = _PlayListControllerBase with _$PlayListController;
 
 abstract class _PlayListControllerBase with Store {
-  void onPressed() {}
+  List<String> playlists = [];
+
+  @observable
+  double heightScreen;
+
+  FocusNode focusNode;
+
+  _PlayListControllerBase(double heightScreen, FocusNode focusNode) {
+    this.heightScreen = heightScreen;
+    this.focusNode = focusNode;
+    getPlaylists();
+
+    this.focusNode.addListener(noFocus);
+  }
+
+  Future<void> getPlaylists() async {
+    playlists =
+        await Modular.get<HomeStore>().getSongsOnDisk(Constants.PLAYLIST_KEY);
+  }
+
+  @action
+  void onTapTextField() {
+    print(heightScreen);
+    heightScreen = heightScreen + heightScreen * 0.35;
+  }
+
+  void noFocus() {
+    if (!focusNode.hasFocus) heightScreen = heightScreen - heightScreen * 0.26;
+  }
 }
--- lib/app/modules/home/pages/playlist/controller/playlist_controller.g.dart
+++ lib/app/modules/home/pages/playlist/controller/playlist_controller.g.dart
@@ -9,10 +9,39 @@ part of 'playlist_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$PlayListController on _PlayListControllerBase, Store {
+  final _$heightScreenAtom = Atom(name: '_PlayListControllerBase.heightScreen');
+
+  @override
+  double get heightScreen {
+    _$heightScreenAtom.reportRead();
+    return super.heightScreen;
+  }
+
+  @override
+  set heightScreen(double value) {
+    _$heightScreenAtom.reportWrite(value, super.heightScreen, () {
+      super.heightScreen = value;
+    });
+  }
+
+  final _$_PlayListControllerBaseActionController =
+      ActionController(name: '_PlayListControllerBase');
+
+  @override
+  void onTapTextField() {
+    final _$actionInfo = _$_PlayListControllerBaseActionController.startAction(
+        name: '_PlayListControllerBase.onTapTextField');
+    try {
+      return super.onTapTextField();
+    } finally {
+      _$_PlayListControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+heightScreen: ${heightScreen}
     ''';
   }
 }
