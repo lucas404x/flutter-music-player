6520d57699e00f0c4bc245540fdec460a6e1f461
Lucas <joselucas0303@gmail.com>
2020-07-08 20:47:38 -0300

    container error. not fixed yet

--- lib/app/modules/home/pages/playlist/playlist_page.dart
+++ lib/app/modules/home/pages/playlist/playlist_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_music_player/app/modules/home/pages/playlist/controller/playlist_controller.dart';
 import 'package:flutter_music_player/app/modules/home/utils/widgets/button/button.dart';
 
@@ -11,74 +10,83 @@ class PlaylistPage extends StatefulWidget {
 class _PlaylistPageState extends State<PlaylistPage> {
   @override
   Widget build(BuildContext context) {
-    final playlistController =
-        PlayListController(MediaQuery.of(context).size.height, FocusNode());
+    final playlistController = PlayListController();
+    Size screenSize = MediaQuery.of(context).size;
 
-    return Column(
-      children: <Widget>[
-        Container(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(20, 50, 0, 60),
-            child: Text(
-              'Playlist',
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 48),
-            ),
-          ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemBuilder: (_, index) => Container(),
-            itemCount: 0,
-            scrollDirection: Axis.horizontal,
-          ),
-        ),
-        Button(
-            height: 52,
-            width: 328,
-            title: 'Create a new playlist',
-            onPressed: () => showModalBottomSheet(
-                context: context,
-                builder: (_) => Observer(
-                      builder: (_) => Container(
-                        height: playlistController.heightScreen,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(30),
-                              topRight: Radius.circular(30),
-                            )),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.all(10),
-                              child: TextField(
-                                focusNode: playlistController.focusNode,
-                                decoration: InputDecoration(
-                                  hintText: 'Playlist name',
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(20, 50, 0, 60),
+                  child: Text(
+                    'Playlist',
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 48),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (_, index) => Container(),
+                  itemCount: 0,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+              Button(
+                height: 52,
+                width: 328,
+                title: 'Create a new playlist',
+                onPressed: () => showModalBottomSheet(
+                  context: context,
+                  builder: (_) => Container(
+                    height: screenSize.height * 0.35,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        )),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: TextField(
+                            controller:
+                                playlistController.textEditingController,
+                            decoration: InputDecoration(
+                              hintText: 'Playlist name',
                             ),
-                            Button(
-                              title: 'Create playlist',
-                              height: 40,
-                              width: 300,
-                              onPressed: () {},
-                            ),
-                            SizedBox(
-                              height: 1,
-                            )
-                          ],
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                      ),
-                    ))),
-        SizedBox(
-          height: 60,
-        )
-      ],
+                        Button(
+                          title: 'Create playlist',
+                          height: 40,
+                          width: 300,
+                          onPressed: () {},
+                        ),
+                        SizedBox(
+                          height: 1,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
