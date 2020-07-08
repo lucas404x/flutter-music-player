95a13ab12609862eea1e6669e4844a2122a27864
Lucas <joselucas0303@gmail.com>
2020-07-08 13:16:56 -0300

    modal button sheet added

--- lib/app/modules/home/pages/playlist/playlist_page.dart
+++ lib/app/modules/home/pages/playlist/playlist_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_music_player/app/modules/home/pages/playlist/controller/playlist_controller.dart';
 import 'package:flutter_music_player/app/modules/home/utils/widgets/button/button.dart';
 
@@ -10,7 +11,8 @@ class PlaylistPage extends StatefulWidget {
 class _PlaylistPageState extends State<PlaylistPage> {
   @override
   Widget build(BuildContext context) {
-    final playlistController = PlayListController();
+    final playlistController =
+        PlayListController(MediaQuery.of(context).size.height, FocusNode());
 
     return Column(
       children: <Widget>[
@@ -20,7 +22,7 @@ class _PlaylistPageState extends State<PlaylistPage> {
             padding: EdgeInsets.fromLTRB(20, 50, 0, 60),
             child: Text(
               'Playlist',
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 48),
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 48),
             ),
           ),
         ),
@@ -32,11 +34,48 @@ class _PlaylistPageState extends State<PlaylistPage> {
           ),
         ),
         Button(
-          height: 52,
-          width: 328,
-          title: 'Create a new playlist',
-          onPressed: playlistController.onPressed,
-        ),
+            height: 52,
+            width: 328,
+            title: 'Create a new playlist',
+            onPressed: () => showModalBottomSheet(
+                context: context,
+                builder: (_) => Observer(
+                      builder: (_) => Container(
+                        height: playlistController.heightScreen * 0.4,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30),
+                            )),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                              child: TextField(
+                                focusNode: playlistController.focusNode,
+                                onTap: playlistController.onTapTextField,
+                                decoration: InputDecoration(
+                                  hintText: 'Playlist name',
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Button(
+                              title: 'Create playlist',
+                              height: 40,
+                              width: 300,
+                              onPressed: () {},
+                            ),
+                            SizedBox(
+                              height: 1,
+                            )
+                          ],
+                        ),
+                      ),
+                    ))),
         SizedBox(
           height: 60,
         )
