95efd3a6121df22abef6f318c8f381c640633c73
Lucas <joselucas0303@gmail.com>
2020-07-09 10:09:03 -0300

    CurrentSongController removed and PlayListController added

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_music_player/app/modules/home/home_page.dart';
-import 'package:flutter_music_player/app/modules/home/pages/current_song/controller/current_song_controller.dart';
+import 'package:flutter_music_player/app/modules/home/pages/playlist/controller/playlist_controller.dart';
 import 'package:flutter_music_player/app/modules/home/stores/home_store.dart';
 
 import 'controller/home_controller.dart';
@@ -9,8 +9,8 @@ class HomeModule extends ChildModule {
   @override
   List<Bind> get binds => [
         Bind((i) => HomeController()),
+        Bind((i) => PlayListController(), lazy: true),
         Bind((i) => HomeStore(i.args.data)),
-        Bind((i) => CurrentSongController()),
       ];
 
   @override
