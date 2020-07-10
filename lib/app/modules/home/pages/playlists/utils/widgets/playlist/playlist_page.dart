1859b3bf6bbeffa386531a19efa7c38ab3cee9aa
Lucas <joselucas0303@gmail.com>
2020-07-10 18:01:01 -0300

    PlaylistPage refactored

--- lib/app/modules/home/pages/playlists/utils/widgets/playlist/playlist_page.dart
+++ lib/app/modules/home/pages/playlists/utils/widgets/playlist/playlist_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_music_player/app/modules/home/models/playlist.dart';
 
 class PlaylistPage extends StatelessWidget {
+  final Playlist playlist;
+
+  PlaylistPage(this.playlist);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Text(playlist.name.toString()),
+      ),
+    );
   }
 }
