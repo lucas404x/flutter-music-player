db369e5b33847dafdc29aa3ff9a9cee2e7e0cb04
Lucas <joselucas0303@gmail.com>
2020-07-10 00:24:32 -0300

    PlaylistController removed from bind list

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_music_player/app/modules/home/home_page.dart';
-import 'package:flutter_music_player/app/modules/home/pages/playlist/controller/playlist_controller.dart';
 import 'package:flutter_music_player/app/modules/home/stores/home_store.dart';
 
 import 'controller/home_controller.dart';
@@ -9,7 +8,6 @@ class HomeModule extends ChildModule {
   @override
   List<Bind> get binds => [
         Bind((i) => HomeController()),
-        Bind((i) => PlayListController()),
         Bind((i) => HomeStore(i.args.data)),
       ];
 
