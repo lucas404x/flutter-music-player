21f790d28a71090d638601f8b6503d41f11c71c9
Lucas <joselucas0303@gmail.com>
2020-07-10 17:57:53 -0300

    PlaylistsController and CurrentSongController added in bind list

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_music_player/app/modules/home/home_page.dart';
+import 'package:flutter_music_player/app/modules/home/pages/current_song/controller/current_song_controller.dart';
+import 'package:flutter_music_player/app/modules/home/pages/playlists/controller/playlists_controller.dart';
 import 'package:flutter_music_player/app/modules/home/stores/home_store.dart';
 
 import 'controller/home_controller.dart';
@@ -9,6 +11,8 @@ class HomeModule extends ChildModule {
   List<Bind> get binds => [
         Bind((i) => HomeController()),
         Bind((i) => HomeStore(i.args.data)),
+        Bind((i) => PlaylistsController(), singleton: false),
+        Bind((i) => CurrentSongController(), singleton: false),
       ];
 
   @override
