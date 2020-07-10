09ab5e877063befd1fef99362543e3ef38fd3cda
Lucas <joselucas0303@gmail.com>
2020-07-10 15:22:52 -0300

    playlist's controller

---
+++ lib/app/modules/home/pages/playlists/utils/widgets/playlist/controller/playlist_controller.dart
@@ -0,0 +1,6 @@
+import 'package:mobx/mobx.dart';
+part 'playlist_controller.g.dart';
+
+class PlaylistController = _PlaylistControllerBase with _$PlaylistController;
+
+abstract class _PlaylistControllerBase with Store {}
