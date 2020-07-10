da2619abb19e2e40871996e23ffd70df78e82085
Lucas <joselucas0303@gmail.com>
2020-07-10 13:56:16 -0300

    size changed and border around it added

--- lib/app/modules/home/pages/playlist/utils/widgets/playlist_card/playlist_card.dart
+++ lib/app/modules/home/pages/playlist/utils/widgets/playlist_card/playlist_card.dart
@@ -8,18 +8,22 @@ class PlayListCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 170.5,
-      height: 187.0,
-      padding: EdgeInsets.all(10),
-      child: Card(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 200.0,
+        height: 187.0,
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.7),
+            border: Border.all(color: Colors.orange)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               playlist.name,
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20),
+              style: TextStyle(fontSize: 24.0),
             )
           ],
         ),
