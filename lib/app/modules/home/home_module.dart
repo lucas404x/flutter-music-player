ef457c41911a177244859d74581d79e1272f5bcf
José Lucas <joselucas0303@gmail.com>
2020-07-11 21:29:09 -0300

    playlist_controller added at bind list

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -2,6 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_music_player/app/modules/home/home_page.dart';
 import 'package:flutter_music_player/app/modules/home/pages/current_song/controller/current_song_controller.dart';
 import 'package:flutter_music_player/app/modules/home/pages/playlists/controller/playlists_controller.dart';
+import 'package:flutter_music_player/app/modules/home/pages/playlists/utils/widgets/playlist/controller/playlist_controller.dart';
 import 'package:flutter_music_player/app/modules/home/stores/home_store.dart';
 
 import 'controller/home_controller.dart';
@@ -11,6 +12,7 @@ class HomeModule extends ChildModule {
   List<Bind> get binds => [
         Bind((i) => HomeController()),
         Bind((i) => HomeStore(i.args.data)),
+        Bind((i) => PlaylistController(), singleton: false),
         Bind((i) => PlaylistsController(), singleton: false),
         Bind((i) => CurrentSongController(), singleton: false),
       ];
