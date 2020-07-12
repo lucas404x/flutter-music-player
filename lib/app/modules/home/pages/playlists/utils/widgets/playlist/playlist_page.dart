b95307ac8584ac97a6b4eaa8b871034f19326492
José Lucas <joselucas0303@gmail.com>
2020-07-12 17:51:51 -0300

    addNewSong function implemented

--- lib/app/modules/home/pages/playlists/utils/widgets/playlist/playlist_page.dart
+++ lib/app/modules/home/pages/playlists/utils/widgets/playlist/playlist_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_music_player/app/modules/home/pages/playlists/utils/widgets/playlist/controller/playlist_controller.dart';
-import 'package:flutter_music_player/app/modules/home/utils/widgets/button/button.dart';
 import 'package:flutter_music_player/app/modules/home/utils/widgets/song_tile/song_tile_widget.dart';
 
 class PlaylistPage extends StatelessWidget {
@@ -12,45 +12,47 @@ class PlaylistPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final playlistController = PlaylistController(playlist);
 
-    return Column(
-      children: <Widget>[
-        Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.only(top: 38.0),
-          child: Container(
-            width: 162.0,
-            height: 162.0,
-            decoration: BoxDecoration(border: Border.all(color: Colors.orange)),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: playlistController.addNewSong,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(top: 38.0),
+            child: Container(
+              width: 162.0,
+              height: 162.0,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.orange)),
+            ),
           ),
-        ),
-        Text(
-          playlist,
-          style: TextStyle(
-              fontSize: (36.0 - playlistController.playlistSongs.length)),
-        ),
-        Text(
-          playlistController.playlistSongs.length > 1
-              ? playlistController.playlistSongs.length.toString()
-              : '0' + playlistController.playlistSongs.length.toString(),
-          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: ListView.builder(
-                itemCount: playlistController.playlistSongs.length + 1,
-                itemBuilder: (_, index) => index == playlist.length + 1
-                    ? SongTile(
-                        songs: playlistController.playlistSongs,
-                        song: playlistController.playlistSongs[index])
-                    : Button(
-                        title: 'Add new song',
-                        width: 328,
-                        height: 52,
-                        onPressed: playlistController.addNewSong)),
+          Text(
+            playlist,
+            style: TextStyle(fontSize: (36.0 - playlist.length)),
           ),
-        )
-      ],
+          Observer(
+            builder: (_) => Text(
+              playlistController.playlistSongs.length.toString(),
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+            ),
+          ),
+          Expanded(
+            child: Observer(
+              builder: (_) => ListView.builder(
+                  itemCount: playlistController.playlistSongs.length,
+                  itemBuilder: (_, index) => SongTile(
+                      songs: playlistController.playlistSongs,
+                      song: playlistController.playlistSongs[index])),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
